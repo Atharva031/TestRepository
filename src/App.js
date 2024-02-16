445fc8e5f6bd169c4ad90b04fe10c7032fe45c26
Atharva Kulkarni <78639678+Atharva031@users.noreply.github.com>
2024-02-16 13:55:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -72,6 +72,236 @@ const financialMetrics = [
   'EBITDA'
 ];
 
+const perShareRevenueMetrics = [
+  'Revenue per Share',
+  'EBITDA per Share',
+  'EBIT per Share',
+  'Earnings per Share (Diluted)',
+  'EPS without NRI',
+  'Owner Earnings per Share (TTM)',
+  'Free Cash Flow per Share',
+  'Operating Cash Flow per Share',
+  'Cash per Share',
+  'Dividends per Share'
+]
+
+const RatiosMetrics = [
+  'ROE %',
+  'ROE % Adjusted to Book Value',
+  'ROA %',
+  'Return-on-Tangible-Equity',
+  'Return-on-Tangible-Asset',
+  'ROC (Joel Greenblatt) %',
+  'ROCE %',
+  '5-Year RORE %',
+  '1-Year ROIIC %',
+  'ROIC %',
+  'WACC %',
+  'Effective Interest Rate on Debt %',
+  'Gross Margin %',
+  'Operating Margin %',
+  'Net Margin %',
+  'EBITDA Margin %',
+  'FCF Margin %',
+  'Debt-to-Equity',
+  'Equity-to-Asset',
+  'Debt-to-Asset',
+  'Liabilities-to-Assets',
+  'Gross-Profit-to-Asset %',
+  'Degree of Financial Leverage',
+  'Asset Turnover',
+  'Dividend Payout Ratio',
+  'Days Sales Outstanding',
+  'Days Payable',
+  'Days Inventory',
+  'Cash Conversion Cycle',
+  'Receivables Turnover',
+  'Inventory Turnover',
+  'COGS-to-Revenue',
+  'Inventory-to-Revenue',
+  'Capex-to-Revenue',
+  'Capex-to-Operating-Income',
+  'Capex-to-Operating-Cash-Flow'
+]
+
+const incomeStatementMetrics = [
+  'Revenue',
+  'Cost of Goods Sold',
+  'Gross Profit',
+  'Gross Margin %',
+  'Selling, General, & Admin. Expense',
+  'Total Operating Expense',
+  'Operating Income',
+  'Operating Margin %',
+  'Interest Income',
+  'Interest Expense',
+  'Net Interest Income',
+  'Other Income (Expense)',
+  'Pretax Income',
+  'Tax Provision',
+  'Tax Rate %',
+  'Other Net Income (Loss)',
+  'Net Income Including Noncontrolling Interests',
+  'Net Income (Continuing Operations)',
+  'Net Income (Discontinued Operations)',
+  'Other Income (Minority Interest)',
+  'Net Income',
+  'Net Margin %',
+  'EPS (Basic)',
+  'EPS (Diluted)',
+  'Shares Outstanding (Diluted Average)',
+  'EBIT',
+  'Depreciation, Depletion and Amortization',
+  'EBITDA',
+  'EBITDA Margin %'
+]
+
+const balanceSheetMetrics = [
+  'Cash and Cash Equivalents',
+  'Cash, Cash Equivalents, Marketable Securities',
+  'Accounts Receivable',
+  'Total Receivables',
+  'Inventories, Other',
+  'Total Inventories',
+  'Other Current Assets',
+  'Total Current Assets',
+  'Land And Improvements',
+  'Buildings And Improvements',
+  'Machinery, Furniture, Equipments',
+  'Construction In Progress',
+  'Other Gross PPE',
+  'Gross Property, Plant and Equipment',
+  'Accumulated Depreciation',
+  'Property, Plant and Equipment',
+  'Intangible Assets',
+  'Goodwill',
+  'Other Long Term Assets',
+  'Total Long-Term Assets',
+  'Total Assets',
+  'Accounts Payable',
+  'Total Tax Payable',
+  'Other Current Payables',
+  'Current Accured Expense',
+  'Accounts Payable & Accured Expense',
+  'Short-Term Debt',
+  'Short-Term Capital Lease Obligation',
+  'Short-Term Debt & Capital Obligation',
+  'Current Deferred Revenue',
+  'Deferred Tax And Revenue',
+  'Other Current Liabilities',
+  'Total Current Liabilities',
+  'Long-Term Debt',
+  'Long-Term Capital Lease Obligation',
+  'Long-Term Debt & Capital Lease Obligation',
+  'Debt-to-Equity',
+  'NonCurrent Deferred Liabilities',
+  'Other Long-Term Liabilities',
+  'Total Long-Term Liabilities',
+  'Total Liabilities',
+  'Common Stock',
+  'Retained Earnings',
+  'Accumulated other comprehensive income (loss)',
+  'Additional Paid-In Capital',
+  'Total Stockholders Equity',
+  'Minority Interest',
+  'Total Equity', 
+  'Equity-to-Asset'
+]
+
+const cashFlowStatementMetrics = [
+  'Net Income From Continuing Operations',
+  'Cash Flow Depreciation, Depletion and Amortization',
+  'Change In Receivables',
+  'Change In Inventory',
+  'Change In Payables And Accured Expense',
+  'Change In Working Capital',
+  'Deferred Tax',
+  'Cash Flow from Others',
+  'Cash Flow from Operations',
+  'Purchase Of Property, Plant, Equipment',
+  'Sale Of Property, Plant, Equipment',
+  'Purchase Of Business',
+  'Sale Of Business',
+  'Cash From Other Investing Activities',
+  'Cash Flow from Investing',
+  'Repurchase of Stock',
+  'Issuance of Debt',
+  'Payments of Debt',
+  'Net Issuance of Debt',
+  'Cash Flow for Dividends',
+  'Other Financing',
+  'Cash Flow from Financing',
+  'Beginning Cash Position',
+  'Effect of Exchange Rate Changes',
+  'Net Change in Cash',
+  'Ending Cash Position',
+  'Capital Expenditure',
+  'Free Cash Flow'
+]
+
+const valuationRatiosStatementMetrics = [
+  'PE Ratio',
+  'PE Ratio without NRI',
+  'Price-to-Owner-Earnings',
+  'PB Ratio',
+  'Price-to-Tangible-Book',
+  'Price-to-Free-Cash-Flow',
+  'Price-to-Operating-Cash-Flow',
+  'PS Ratio',
+  'PEG Ratio',
+  'EV-to-Revenue',
+  'EV-to-EBITDA',
+  'EV-to-EBIT',
+  'Earnings Yield (Joel Greenblatt) %',
+  'Forward Rate of Return (Yacktman) %',
+  'Shiller PE Ratio',
+  'Cyclically Adjusted PB Ratio',
+  'Cyclically Adjusted PS Ratio',
+  'Cyclically Adjusted Price-to-FCF',
+  'Dividend Yield %',
+  'FCF Yield %'
+]
+
+const valuationAndQualityMetrics = [
+  'Market Cap',
+  'Enterprise Value',
+  'Month End Stock Price',
+  'Net Cash per Share',
+  'Net Current Asset Value',
+  'Net-Net Working Capital',
+  'Intrinsic Value: Projected FCF',
+  'Median PS Value',
+  'Peter Lynch Fair Value',
+  'Graham Number',
+  'Earnings Power Value(EPV)',
+  'Altman Z-Score',
+  'Piotroski F-Score',
+  'Beneish M-Score',
+  'Scaled Net Operating Assets',
+  'Sloan Ratio %',
+  'Current Ratio',
+  'Quick Ratio',
+  'Cash Ratio',
+  'Interest Coverage',
+  'Highest Stock Price',
+  'Lowest Stock Price',
+  'Shares Buyback Ratio %',
+  'Buyback Yield %',
+  'YoY Rev. per Sh. Growth',
+  'YoY EPS Growth',
+  'YoY EBITDA Growth (%)',
+  '5-Year EBITDA Growth Rate (Per Share)',
+  'Shares Outstanding (Basic Average)',
+  'Shares Outstanding (EOP)',
+  'Beta',
+  'Reinstated Filing Date',
+  'Filing Date',
+  'Earnings Release Date',
+  'Number of Shareholders',
+  'Number of Employees',
+  'Forex Rate'
+]
+
 const financialStrengthMetrics = [
   'Cash to Debt',
   'Equity to Asset',
@@ -156,6 +386,9 @@ const GFValueRankMetrics = [
 ]
 
 function TableRows({ data, label }) {
+  // Check if data is undefined or null, return null if so
+  if (!data) return null;
+
   return (
     <tbody>
       <tr>
@@ -437,12 +670,6 @@ function DividendSummary({ label }) {
   }
  
 
-
-
-
-
-
-
 function App() {
   return (
     <div className='App'>
@@ -511,8 +738,148 @@ function App() {
 
         <h1>Key Ratios (Quarterly)</h1>
         <table>
-          {keyRatiosMetrics.slice(1).map((metric,index) => (
+          {keyRatiosMetrics.map((metric,index) => (
             <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.valuation_ratios[metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Per Share Data (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {perShareRevenueMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.per_share_data_array[metric]} />
+          ))}
+        </table>
+
+        <h1>Per Share Data (Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {perShareRevenueMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.per_share_data_array[metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Ratios (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {RatiosMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.common_size_ratios[metric]} />
+          ))}
+        </table>
+
+        <h1>Ratios (Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {RatiosMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.common_size_ratios[metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Income Statement (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {incomeStatementMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.income_statement[metric]} />
+          ))}
+        </table>
+
+        <h1>Income Statement (Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {incomeStatementMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.income_statement[metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Balance Sheet (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {balanceSheetMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.balance_sheet [metric]} />
+          ))}
+        </table>
+
+        <h1>Balance Sheet(Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {balanceSheetMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.balance_sheet[metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Cashflow Statement (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {cashFlowStatementMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.cashflow_statement [metric]} />
+          ))}
+        </table>
+
+        <h1>Cashflow Statement (Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {cashFlowStatementMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.cashflow_statement [metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Valuation Ratio (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {valuationRatiosStatementMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.valuation_ratios [metric]} />
+          ))}
+        </table>
+
+        <h1>Valuation Ratio (Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {valuationRatiosStatementMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.valuation_ratios [metric]} />
+          ))}
+        </table>
+      </div>
+
+      <div className='Table'>
+        <h1>Valuation and Quality (Annually)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearAnnual
+        } />
+          {valuationAndQualityMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={annualsProfitAndLoss.valuation_and_quality [metric]} />
+          ))}
+        </table>
+
+        <h1>Valuation and Quality (Quarterly)</h1>
+        <table>
+          <TableRows label="Fiscal Year" data={fiscalYearQuarter
+        } />
+          {valuationAndQualityMetrics.map((metric, index) => (
+            <TableRows key={index} label={metric} data={quarterlyProfitAndLoss.valuation_and_quality [metric]} />
           ))}
         </table>
       </div>
@@ -579,9 +946,6 @@ function App() {
           ))}
         </table> 
       </div>
-
-
-
     </div>
   );
 }
